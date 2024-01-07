25bbefd0167d131627aab2fa020b350af3d7c39f
Nekoyama141592 <mtaky0904@gmail.com>
2024-01-07 22:22:49 +0900

    アプリとFirebaseを接続しよう

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:text_sns/flavors.dart';
 import 'app.dart';
+import 'package:text_sns/gen/firebase_options_dev.dart' as dev;
+import 'package:text_sns/gen/firebase_options_prod.dart' as prod;
+import 'package:firebase_core/firebase_core.dart';
 
 FutureOr<void> main() async {
+  final flavor = F.appFlavor!;
+  final options = (flavor == Flavor.dev) ? dev.DefaultFirebaseOptions.currentPlatform 
+  : prod.DefaultFirebaseOptions.currentPlatform;
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: options);
   runApp(const App());
 }
