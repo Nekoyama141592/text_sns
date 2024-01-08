b5be75e8f5cb7e1a67258ecc548a3ce4cf7f4f0b
Nekoyama141592 <mtaky0904@gmail.com>
2024-01-08 15:51:09 +0900

    FirestoreのClientを作成しよう

---
+++ lib/infrastructure/firestore/firestore_client.dart
@@ -0,0 +1,6 @@
+import 'package:text_sns/typedefs/firestore_typedef.dart';
+
+class FirestoreClient {
+  Future<void> createDoc(DocRef ref,SDMap data) async => await ref.set(data);
+  FutureDoc getDoc(DocRef ref) async => await ref.get();
+}
