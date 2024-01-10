5a042c2dd98e02d89d1b345523dab5e0b8bd5f8e
Nekoyama141592 <mtaky0904@gmail.com>
2024-01-10 10:07:19 +0900

    AuthRepositoryを作成しよう

---
+++ lib/repository/auth_repository.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:text_sns/infrastructure/auth/auth_client.dart';
+import 'package:text_sns/models/result/result.dart';
+import 'package:text_sns/typedefs/result_typedef.dart';
+
+class AuthRepository {
+  FutureResult<User> createUserWithEmailAndPassword(
+      String email, String password) async {
+    final client = AuthClient();
+    try {
+      final res = await client.createUserWithEmailAndPassword(email, password);
+      final user = res.user;
+      return (user == null) ? const Result.failure() : Result.success(user);
+    } catch (e) {
+      debugPrint(e.toString());
+      return const Result.failure();
+    }
+  }
+
+  FutureResult<User> signInWithEmailAndPassword(
+      String email, String password) async {
+    final client = AuthClient();
+    try {
+      final res = await client.signInWithEmailAndPassword(email, password);
+      final user = res.user;
+      return (user == null) ? const Result.failure() : Result.success(user);
+    } catch (e) {
+      debugPrint(e.toString());
+      return const Result.failure();
+    }
+  }
+}
