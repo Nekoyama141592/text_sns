db8dec9768a9fa1bdbc81cd2c467cbc08eeeee44
Nekoyama141592 <mtaky0904@gmail.com>
2024-01-11 21:02:32 +0900

    色を整えよう

--- lib/app/themes.dart
+++ lib/app/themes.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:text_sns/constant/colors.dart';
 
 class Themes {
-  static ThemeData lightTheme() => ThemeData.light()
-      .copyWith(appBarTheme: const AppBarTheme(color: Colors.green));
-  static ThemeData dartTheme() => ThemeData.dark()
-      .copyWith(appBarTheme: const AppBarTheme(color: Colors.black));
+  static ThemeData lightTheme() => ThemeData.light().copyWith(
+      primaryColor: kPrimaryColorLightTheme,
+      scaffoldBackgroundColor: kContentColorLightTheme,
+      appBarTheme: const AppBarTheme(color: kPrimaryColorLightTheme));
+  static ThemeData dartTheme() => ThemeData.dark().copyWith(
+      primaryColor: kPrimaryColorDarkTheme,
+      scaffoldBackgroundColor: kContentColorDarkTheme.withOpacity(0.87),
+      appBarTheme: const AppBarTheme(color: kPrimaryColorDarkTheme));
 }
---
+++ lib/constant/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColorDarkTheme = Color(0xFF0066ff);
+const kPrimaryColorLightTheme = Color(0xFF00BF6D);
+const kContentColorDarkTheme = Color(0xFF1D1D35);
+const kContentColorLightTheme = Color(0xFFF5FCF9);
