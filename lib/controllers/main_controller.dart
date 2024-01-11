a2b81e585553c6237521359e21e3959cc4620e34
Nekoyama141592 <mtaky0904@gmail.com>
2024-01-11 10:29:56 +0900

    公開ユーザーを作成しよう

---
+++ lib/controllers/main_controller.dart
@@ -0,0 +1,48 @@
+import 'package:get/get.dart';
+import 'package:text_sns/controllers/auth_controller.dart';
+import 'package:text_sns/core/firestore/doc_ref_core.dart';
+import 'package:text_sns/models/public_user/public_user.dart';
+import 'package:text_sns/repository/firestore_repository.dart';
+import 'package:text_sns/typedefs/firestore_typedef.dart';
+import 'package:text_sns/ui_core/ui_helper.dart';
+
+class MainController extends GetxController {
+  final rxPublicUser = Rx<PublicUser?>(null);
+  @override
+  void onInit() async {
+    await _manageUserInfo();
+    super.onInit();
+  }
+
+  Future<void> _manageUserInfo() async {
+    final authUser = AuthController.to.rxAuthUser.value;
+    if (authUser == null) return;
+    final repository = FirestoreRepository();
+    final uid = authUser.uid;
+    final ref = DocRefCore.publicUserDocRef(uid);
+    final result = await repository.getDoc(ref);
+    result.when(success: (res) async {
+      final json = res.data();
+      if (res.exists && json != null) {
+        rxPublicUser.value = PublicUser.fromJson(json);
+      } else {
+        await _createPublicUser(ref, uid);
+      }
+    }, failure: () {
+      UIHelper.showFlutterToast("ユーザーの読み取りが失敗しました");
+    });
+  }
+
+  Future<void> _createPublicUser(DocRef ref, String uid) async {
+    final repository = FirestoreRepository();
+    final newPublicUser = PublicUser(uid: uid);
+    final json = newPublicUser.toJson();
+    final result = await repository.createDoc(ref, json);
+    result.when(success: (_) {
+      rxPublicUser.value = newPublicUser;
+      UIHelper.showFlutterToast("ユーザーの作成が成功しました");
+    }, failure: () {
+      UIHelper.showFlutterToast("ユーザーの作成が失敗しました");
+    });
+  }
+}
--- lib/view/pages/my_home_page/components/main_screen.dart
+++ lib/view/pages/my_home_page/components/main_screen.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:text_sns/controllers/auth_controller.dart';
+import 'package:text_sns/controllers/main_controller.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final mainController = Get.put(MainController());
     final authController = AuthController.to;
     const style = TextStyle(fontSize: 30.0);
-    return ElevatedButton(
-        onPressed: authController.onSignOutButtonPressed,
-        child: const Text(
-          "ログアウトする",
-          style: style,
-        ));
+    return Column(
+      children: [
+        Obx(
+          () => Text(
+            mainController.rxPublicUser.value?.uid ?? "Nullです",
+            style: style,
+          ),
+        ),
+        const SizedBox(
+          height: 20.0,
+        ),
+        ElevatedButton(
+            onPressed: authController.onSignOutButtonPressed,
+            child: const Text(
+              "ログアウトする",
+              style: style,
+            )),
+      ],
+    );
   }
 }
